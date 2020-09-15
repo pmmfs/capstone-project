34b4c0f2acd5abc0b529c1b49f25868596fde9ca
Femi Adeleke <femolyn1@yahoo.co.uk>
2020-09-15 14:33:05 -0500

    Added more features to map

--- regulation/map/static/js/logic.js
+++ regulation/map/static/js/logic.js
@@ -87,8 +87,9 @@ pointToLayer: function(feature, latlng) {
   }).addTo(all_locations);
 // Adding the all locations layer to our map
   all_locations.addTo(map);
+
 // Reading total death data
-  d3.json("https://raw.githubusercontent.com/pmmfs/capstone-project/FA-2/regulation/Resources/Top5TotalDeaths.json").then(function(data) {
+  d3.json("https://raw.githubusercontent.com/pmmfs/capstone-project/FA-2/regulation/Resources/Top5TotalDeaths.geojson").then(function(data) {
     var geojsonMarkerOptions = {
       radius: 10,
       fillColor: "blue",
@@ -108,25 +109,44 @@ pointToLayer: function(feature, latlng) {
          // We create a popup for each circleMarker to display the magnitude and
       //  location of the earthquake after the marker has been created and styled.
       onEachFeature: function(feature, layer) {
-          layer.bindPopup("Continent: " + feature.properties.continent + "<br>Country: " + feature.properties.location);
+          layer.bindPopup("Country: " + feature.properties.location + "<br>Deaths: " + feature.properties.total_deaths_updated);
         }
       
-      }).addTo(all_locations);
+      }).addTo(totaldeaths_top5locations);
     // Adding the all locations layer to our map
-      all_locations.addTo(map);
+      totaldeaths_top5locations.addTo(map);
     
+  // Reading the top 5 stringency index data
+  d3.json("https://raw.githubusercontent.com/pmmfs/capstone-project/FA-2/regulation/Resources/Top5StringencyIndex.geojson").then(function(data) {
+    var geojsonMarkerOptions = {
+      radius: 10,
+      fillColor: "green",
+      color: "white",
+      weight: 1,
+      opacity: 1,
+      fillOpacity: 0.8
+    };
     
+    // Creating a GeoJSON layer with the retrieved data.
+    L.geoJson(data, {
+    // We turn each feature into a circleMarker on the map.
+    pointToLayer: function(feature, latlng) {
+                  return L.circleMarker(latlng, geojsonMarkerOptions);
+                   
+            },
+         // We create a popup for each circleMarker to display the SI and
+     
+      onEachFeature: function(feature, layer) {
+          layer.bindPopup("Country: " + feature.properties.location + "<br>Stringency Index: " + feature.properties.StringencyIndex_updated);
+        }
+      
+      }).addTo(top5_StringencyIndex);
+    // Adding Stringenecy Index  layer to our map
+      top5_StringencyIndex.addTo(map);
+    
+    })
+  })
 
 
 
-
-
-
-
-
-
-
-
-
-}); 
-
+});
