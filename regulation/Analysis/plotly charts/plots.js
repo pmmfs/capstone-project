acd6ac5d16e0c1f0f337bed43a8d9acac3541e6b
Muzznah Ansari <muzznahansari@gmail.com>
2020-09-18 16:56:40 -0400

    Synchronizing feature variables with the final version

--- regulation/Analysis/plotly charts/plots.js
+++ regulation/Analysis/plotly charts/plots.js
@@ -1,7 +1,8 @@
 // ____________________Total Deaths______________________________
 var trace = {
-    labels: ['Population','Population Density','Median Age','Total Cases','StringencyIndex'],
-    values: [2.71,  1.70,  3.57, 83.08,  8.95],
+    labels: ['Population','Population Density','Median Age','Total Cases','EconomicSupportIndex','ContainmentHealthIndex','StringencyIndex'],
+    values: [ 2.32,  0.50,  3.17, 83.23,  1.97,
+        0.40,  8.41],
     marker:{
         colors:['gold', 'mediumturquoise', 'darkorange','teal','lightgreen','coral','tomato']},
     'type': 'pie'    
@@ -16,8 +17,9 @@ Plotly.newPlot("plotArea-TD", data, layout);
 // ___________________Total Cases_______________________________________
 
 var trace1 = {
-    labels: ['Date','Population','Population Density','Median Age','EconomicSupportIndex','ContainmentHealthIndex','StringencyIndex'],
-    values: [30.4, 20.61, 20.95, 5.62, 8.6, 6.82, 7.0],
+    labels: ['Population','Population Density','Median Age','EconomicSupportIndex','ContainmentHealthIndex','StringencyIndex'],
+    values: [20.22,  2.79,  6.77 , 25.12, 19.18,
+        25.91713592],
     marker:{
         colors:['gold', 'mediumturquoise', 'darkorange', 'coral','tomato','teal','lightgreen']},
     'type': 'pie'    
@@ -32,8 +34,9 @@ Plotly.newPlot("plotArea-TC", data1, layout1);
 // ______________________New Deaths______________________________________________
 
 var trace2 = {
-    labels: ['Date','Population','Population Density','Median Age','New Cases','StringencyIndex'],
-    values: [13.23, 1.94, 2.7, 3.25, 75.27, 3.61],
+    labels: ['Population','Population Density','Median Age','New Cases','Total Deaths','EconomicSupportIndex','ContainmentHealthIndex','StringencyIndex'],
+    values: [0.95,  1.99,  1.42, 74.81, 12.57,
+        2.34,  2.92,  3.01],
     marker:{
         colors:['gold', 'mediumturquoise', 'darkorange', 'coral','tomato','teal','lightgreen']},
     'type': 'pie'    
@@ -48,8 +51,9 @@ Plotly.newPlot("plotArea-ND", data2, layout2);
 // ______________________New Cases______________________________________________
 
 var trace3 = {
-    labels: ['Date Updated','Population','Population Density','Median Age','EconomicSupportIndex','ContainmentHealthIndex','StringencyIndex'],
-    values: [26.6, 23.5, 3.07, 14.51, 28.08, 2.85, 1.39],
+    labels: ['Population','Population Density','Median Age','EconomicSupportIndex','ContainmentHealthIndex','StringencyIndex'],
+    values: [24.36,  2.94,  9.94, 31.15, 19.49 ,
+        12.13],
     marker:{
         colors:['gold', 'mediumturquoise', 'darkorange', 'coral','tomato','teal','lightgreen']},
     'type': 'pie'    
@@ -61,42 +65,3 @@ var layout3 = {
    };
 Plotly.newPlot("plotArea-NC", data3, layout3);
 
-// _______________________________STRINGENCY INDEX COMPONENT____________________________________
-
-// ____________________Total Deaths______________________________
-var trace4 = {
-    labels: ['School closing',
-    'Workplace closing','Cancel public events','Restrictions on gatherings','Close public transport',
-    'Stay at home requirements','Restrictions on internal movement','International travel controls', 
-     'Public information campaigns'],
-    values: [8.64, 11.32,  8.44,  7.08,  5.33,
-        14.62, 20.13, 21.17,  3.27],
-    marker:{
-        colors:['gold', 'mediumturquoise', 'darkorange','teal','lightgreen','coral','tomato']},
-    'type': 'pie'    
-   };
-var data4 = [trace4];
-var layout4 = {
-    title: "Feature Importance for Total Death",
-    
-   };
-Plotly.newPlot("plotArea-STD", data4, layout4);
-
-// ___________________Total Cases_______________________________________
-
-var trace1 = {
-    labels: ['School closing','Workplace closing','Cancel public events','Restrictions on gatherings',
-    'Close public transport','Stay at home requirements','Restrictions on internal movement',
-    'International travel controls','Public information campaigns'],
-    values: [8.44 , 12.00 ,  7.78,  7.33,  5.45,
-        14.71, 19.88, 21.24,  3.19],
-    marker:{
-        colors:['gold', 'mediumturquoise', 'darkorange', 'coral','tomato','teal','lightgreen']},
-    'type': 'pie'    
-   };
-var data1 = [trace1];
-var layout1 = {
-    title: "Feature Importance for Total Cases",
-    
-   };
-Plotly.newPlot("plotArea-STC", data1, layout1);
